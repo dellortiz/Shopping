c82324fd266181f310e192358096a786b842d344
Jose Luis Portillo <jo6024934@gmail.com>
2025-02-07 01:07:01 +0100

    Added new page:  about_online_shopping

---
+++ about_online_shopping.php
@@ -0,0 +1,50 @@
+<?php include_once("./common/header.php")?>
+<main class="main-body-index">
+    <div class="div-learn">
+<h1 class="p-main1"> Learn more about online shopping</h1>
+
+<h2>How does online shopping work?</h2>
+<p>An online store is a website designed with the objective of selling products or services through electronic commerce tools. 
+It works through a product catalog, a shopping 
+cart and an online payment system, which together facilitate digital transactions.</p>
+
+<h2>What are the advantages or disadvantages of online shopping?</h2>
+<p>Consider the pros and cons of shopping online: convenience, wider selection, and deals versus security concerns and lack of physical interaction.</p>
+
+<h2>What does selling online entail?</h2>
+<p>Electronic commerce or ecommerce is the trade of goods and services on the Internet. The Internet allows individuals 
+    and businesses to buy and sell an increasing number of physical goods, digital goods, and services electronically.</p>
+
+<h2>What are the elements of a virtual store?</h2>
+<h3>Essential elements that a virtual store must have</h3>
+<ol>
+<li>Logo, slogan and name.</li>
+<li>Product catalog with their categories (if necessary).</li>
+<li>Shopping cart.</li>
+<li>Payment gateway with a wide offer.</li>
+<li>Legal information, policies and privacy notice.</li>
+<li>Product finder.</li>
+<li>Payment methods.</li>
+<li>Shipping methods.</li>
+</ol>
+</div>
+<div class="bloque2">
+  <div class="contenedor-slider">
+    <i class="fa-solid fa-chevron-left fa-2x" onclick="cambio(-1)"></i>
+    <div class="slide-container" id="slide-container">
+      <!-- Las imágenes se cargarán dinámicamente -->
+    </div>
+    <i class="fa-solid fa-chevron-right fa-2x" onclick="cambio(1)"></i>
+  </div>
+</div>
+</main>
+    <script src="./asset/js/pannier.js"></script>
+    <script src="./asset/js/signin.js"></script>
+    <script src="./asset/js/script.js"></script>
+    <script src="./asset/js/slider.js"></script>
+  
+    <footer>
+
+    </footer>
+</body>
+</html>
